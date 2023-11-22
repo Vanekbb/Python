d6c93a5457d051d0c49551bf88106ed66f2f1f92
vanek <ivan2005ab@gmail.com>
2023-11-22 11:19:51 +0300

    task2 done

---
+++ task2/task2.py
@@ -0,0 +1,26 @@
+'''
+
+Пользователь вводит текст(строка). Словом считается
+последовательность непробельных символов идущих
+подряд, слова разделены одним или большим числом
+пробелов. Определите, сколько различных слов
+содержится в этом тексте.
+
+Input: She sells sea shells on the sea shore The shells
+that she sells are sea shells I'm sure So if she sells sea
+shells on the sea shore I'm sure that the shells are sea
+shore shells
+
+Output: 13
+
+'''
+
+userText = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+
+lowerText = userText.lower()
+
+newText = lowerText.split()
+
+setWords = set(newText)
+
+print(len(setWords))
