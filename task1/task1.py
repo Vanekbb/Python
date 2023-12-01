21215b6254f80f8ea70b87faf546a298f0ad04d6
vanek <ivan2005ab@gmail.com>
2023-12-01 16:08:31 +0300

    task1 done

---
+++ task1/task1.py
@@ -0,0 +1,23 @@
+'''
+
+Есть код:
+
+transformation = <???>
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+transformed_values = list(map(transformation, values))
+
+if values == transformed_values:
+    print(‘ok’)
+else:
+        print(‘fail’)
+
+'''
+
+transformation = lambda x: x
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+transformed_values = list(map(transformation, values))
+
+if values == transformed_values:
+    print("ok")
+else:
+        print("fail")
